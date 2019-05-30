94e39be692452bdbdecbe3796638d4ccc3c7a9eb
Marius Steller Imingen <marius.steller@gmail.com>
2019-05-30 21:42:38 +0200

    mkjl

--- 8080Disassembler.py
+++ 8080Disassembler.py
@@ -1,6 +1,13 @@
 
 class State8080():
-    def __init__():
+    def __init__(self):
+        self.a = 0
+        self.b = 0
+        self.c = 0
+        self.d = 0
+        self.e = 0
+        self.h = 0
+        self.l = 0
         self.sp = 0
         self.pc = 0
         self.memory = []
@@ -15,7 +22,7 @@ class State8080():
 class ConditionCodes():
     '''Contains different flags to help the program
     '''
-    def __init__():
+    def __init__(self):
         self.z = 1 # (zero) set to 1 when the result is equal to zero
         self.s = 1 # (sign) set to 1 when bit 7 (the most significant bit or MSB) of the math instruction is set
         self.p = 1 # (parity) is set when the answer has even parity, clear when odd parity
@@ -24,39 +31,65 @@ class ConditionCodes():
         self.pad = 3 # Dunno yet
 
 def emulate_8080(state):
-    opcode = state.memory[state.pc]
-
-    if opcode == 0x00: # NOP
+    #opcode = state.memory[state.pc]
+    opcode = "0x81"
+    if opcode == "0x0": # NOP
+        print(opcode)
         pass
-    if opcode == 0x01: # B <- byte 3, C <- byte 2
+    if opcode == "0x01": # B <- byte 3, C <- byte 2
+        print(opcode)
         state.c = opcode[1]
         state.b = opcode[2]
         state.pc += 2
         pass
-    if opcode == 0x41:
+    if opcode == "0x41":
+        print(opcode)
         state.b = state.c #MOV B,C
-    if opcode == 0x42:
+    if opcode == "0x42":
+        print(opcode)
         state.b = state.d #MOV B,D
-    if opcode == 0x43:
+    if opcode == "0x43":
+        print(opcode)
         state.b = state.e #MOV B,E
-    if opcode == 0x80: # ADD B
+    if opcode == "0x80": # ADD B
+        print(opcode)    
         answer = state.a + state.b
-        # If answer is zero set flag to zero, else clear flag
+        # ZERO FLAG: if answer is zero set flag to ONE, else clear flag
         if (answer and 0xff) == 0:
             state.cc.z = 1
         else:
             state.cc.z = 0
- 
+        # SIGN FLAG: if bit 7 is set, set the sign flag, else clear it
+        if (answer and 0x80):
+            state.cc.s = 1
+        else:
+            state.cc.s = 0
+        # CARRY FLAG:
+        if (answer > 0xff):  
+            state.cc.cy = 1;    
+        else:    
+            state.cc.cy = 0;    
+        # Parity is handled by a subroutine    
+        #state.cc.p = Parity(answer & 0xff)
+        state.a = answer & 0xff
+    if opcode == "0x81":
+            answer = 0#state.a + state.c    
+            state.cc.z = 1 if ((answer & 0xff) == 0) else 0
+            state.cc.s = 1 if ((answer & 0x80) != 0) else 0   
+            state.cc.cy = 1 if (answer > 0xff) else 0
+            #state.cc.p = Parity(answer&0xff)  
+            state.a = answer & 0xff   
 
-def loadBinaryFile():
+def loadBinaryFile(state):
     # load file.
-    with open("rom\invaders.h", "rb") as f:
+    with open("rom\invaders", "rb") as f:
         fileBuffer = f.read()
 
     # convert binary list to hex.
     hexList = []
     for i in fileBuffer:
         hexList.append(hex(i))
+    state.memory = hexList
     return hexList
 
 
@@ -68,7 +101,7 @@ def test(hexList):
 
         if opCode == hex(0):
             prettyPrint(opCode, "NOP", 1, counter)
-        elif opCode == 0xc3:
+        elif opCode == "0xc3":
             prettyPrint(opCode, "JMP", 3, counter)
         else:
             print("its not")
@@ -83,7 +116,11 @@ def strip0x(s):
     return s[:2]
 
 if __name__ == "__main__":
-    hexList = loadBinaryFile()
+    state = State8080()
+    hexList = loadBinaryFile(state)
+    emulate_8080(state)
+    print(state.cc.z)
 
-    test(hexList)
+
+    #test(hexList)
 
